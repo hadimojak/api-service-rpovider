db6a92b396f065fde85d8099ed35370b70f7d0b9
hadi arbabi <hadimojak@gmail.com>
2026-06-09 15:27:15 +0330

    docker compose added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen({ port, host: '0.0.0.0' });
+
+  console.log(`API running on http://localhost:${port}`);
 }
 bootstrap();
--- src/model/configuration.schema.ts
+++ src/model/configuration.schema.ts
@@ -1,8 +1,13 @@
 import * as Joi from 'joi';
 
 export const ConfigValidationSchema = Joi.object({
-  NODE_ENV: Joi.string().valid('dev', 'stage', 'prod').default('dev'),
   PORT: Joi.number().default(3000),
+
   HOST_REDIS: Joi.string().required(),
+  REDIS_PORT: Joi.number().default(6379),
+  REDIS_PASS: Joi.string().required(),
+
   RABBITMQ_URL: Joi.string().required(),
+  RABBITMQ_USER: Joi.string().required(),
+  RABBITMQ_PASS: Joi.string().required(),
 });
